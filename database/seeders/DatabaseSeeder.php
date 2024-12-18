183d0c1441d7c49528da41b12daf24ce5d0ca3e1
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-12-17 18:59:30 -0700

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,13 @@ use App\Models\Shop\Customer;
 use App\Models\Shop\Order;
 use App\Models\Shop\OrderItem;
 use App\Models\Shop\Product;
+use App\Models\User;
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -22,41 +28,91 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Shop
-        $brands = Brand::factory(20)->create();
+        DB::raw('SET time_zone=\'+00:00\'');
 
-        $categories = ShopCategory::factory(20)
+        // Admin
+        $this->command->warn(PHP_EOL . 'Creating admin user...');
+        $user = $this->withProgressBar(1, fn () => User::factory(1)->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]));
+        $this->command->info('Admin user created.');
+
+        // Shop
+        $this->command->warn(PHP_EOL . 'Creating shop brands...');
+        $brands = $this->withProgressBar(20, fn () => Brand::factory(1)
+            ->create());
+        Brand::query()->update(['sort' => new Expression('id')]);
+        $this->command->info('Shop brands created.');
+
+        $this->command->warn(PHP_EOL . 'Creating shop categories...');
+        $categories = $this->withProgressBar(20, fn () => ShopCategory::factory(1)
             ->has(
                 ShopCategory::factory()->count(3),
                 'children'
             )
-            ->create();
+            ->create());
+        $this->command->info('Shop categories created.');
 
-        $customers = Customer::factory(1000)->create();
+        $this->command->warn(PHP_EOL . 'Creating shop customers...');
+        $customers = $this->withProgressBar(1000, fn () => Customer::factory(1)
+            ->create());
+        $this->command->info('Shop customers created.');
 
-        $products = Product::factory(50)
+        $this->command->warn(PHP_EOL . 'Creating shop products...');
+        $products = $this->withProgressBar(50, fn () => Product::factory(1)
             ->sequence(fn ($sequence) => ['shop_brand_id' => $brands->random(1)->first()->id])
             ->hasAttached($categories->random(rand(3, 6)), ['created_at' => now(), 'updated_at' => now()])
-            ->create();
+            ->create());
+        $this->command->info('Shop products created.');
 
-        $orders = Order::factory(1000)
+        $this->command->warn(PHP_EOL . 'Creating shop orders...');
+        $orders = $this->withProgressBar(1000, fn () => Order::factory(1)
             ->sequence(fn ($sequence) => ['shop_customer_id' => $customers->random(1)->first()->id])
             ->has(
                 OrderItem::factory()->count(rand(2, 5))
                     ->state(fn (array $attributes, Order $order) => ['shop_product_id' => $products->random(1)->first()->id]),
                 'items'
             )
-            ->create();
+            ->create());
+        $this->command->info('Shop orders created.');
 
         // Blog
-        $blogCategories = BlogCategory::factory(20)->create();
+        $this->command->warn(PHP_EOL . 'Creating blog categories...');
+        $blogCategories = $this->withProgressBar(20, fn () => BlogCategory::factory(1)
+            ->create());
+        $this->command->info('Blog categories created.');
 
-        Author::factory(20)
+        $this->command->warn(PHP_EOL . 'Creating blog authors and posts...');
+        $this->withProgressBar(20, fn () => Author::factory(1)
             ->has(
                 Post::factory(5)
                     ->state(fn (array $attributes, Author $author) => ['blog_category_id' => $blogCategories->random(1)->first()->id]),
                 'posts'
             )
-            ->create();
+            ->create());
+        $this->command->info('Blog authors and posts created.');
+    }
+
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection;
+
+        foreach (range(1, $amount) as $item) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
