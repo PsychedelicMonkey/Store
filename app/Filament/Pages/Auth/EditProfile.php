8a191a9e162aa8c314c95932328aad352a7fc9f6
PsychedelicMonkey <ryan.getz.96@gmail.com>
2024-12-22 11:16:43 -0700

    Profile labele

--- app/Filament/Pages/Auth/EditProfile.php
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -2,12 +2,21 @@
 
 namespace App\Filament\Pages\Auth;
 
+use App\Models\User;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Form;
 use Filament\Pages\Auth\EditProfile as BaseEditProfile;
 
 class EditProfile extends BaseEditProfile
 {
+    public static function getLabel(): string
+    {
+        /** @var User $user */
+        $user = auth()->user();
+
+        return $user->name;
+    }
+
     public function form(Form $form): Form
     {
         return $form
